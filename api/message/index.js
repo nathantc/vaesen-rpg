0a8bda61c4b7a1ae5aa11023aaa339ed36c95964
Nathan Carver <nathan@codecarver.io>
2022-12-11 14:49:39 -0500

    Implementing basic Azure function with hello message.

---
+++ api/message/index.js
@@ -0,0 +1,16 @@
+module.exports = async function (context, req) {
+  context.log('HTTP trigger function processed a request.', JSON.stringify(req));
+  const name = (req.query.name || (req.body && req.body.name));
+  const responseMessage = name
+    ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
+    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: {
+      input: name,
+      message: responseMessage
+    }
+
+  };
+};
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
   return (
@@ -6,8 +7,23 @@ function App() {
       <header className="App-header">
         Welcome to Vaesen RPG
       </header>
+      <ApiMessage></ApiMessage>
     </div>
   );
+}
+
+function ApiMessage() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('/api/message');
+      const json = await data.json();
+      setData(json.message);
+    })();
+  }, []);
+
+  return <span>{data}</span>
 }
 
 export default App;
