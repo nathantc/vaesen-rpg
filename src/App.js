d3ce6b9005cdc082f5167912e70fed293d632c02
Nathan Carver <nathan@codecarver.io>
2022-12-11 15:41:42 -0500

    Setting up AD auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 
+const apiHost = window.location.hostname === 'localhost' ? 'http://localhost:7071' : '';
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,7 @@ function App() {
         Welcome to Vaesen RPG
       </header>
       <ApiMessage></ApiMessage>
+      <UserInfo></UserInfo>
     </div>
   );
 }
@@ -17,13 +20,35 @@ function ApiMessage() {
 
   useEffect(() => {
     (async () => {
-      const data = await fetch('/api/message');
-      const json = await data.json();
-      setData(json.message);
+      try {
+        const data = await fetch(`${apiHost}/api/message`);
+        const json = await data.json();
+        setData(json.message);
+      } catch (e) {
+        console.log(e);
+      }
     })();
   }, []);
 
-  return <span>{data}</span>
+  return <div>Message: {data}</div>
+}
+
+function UserInfo() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`${apiHost}/.auth/me`)
+        const json = await response.json();
+        setUser(json.message);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
+
+  return <div>User: {user}</div>
 }
 
 export default App;
