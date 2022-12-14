398e61a9b901050a5d4d5cedc20b34d8df6c3620
Nathan Carver <nathan.carver@bionautlabs.com>
2022-12-14 14:32:20 -0800

    Updating the routes for login/logout based on principal.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
+import {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {PropTypes} from 'prop-types';
 import './App.css';
 
 const apiPath = '';// window.location.hostname === 'localhost' ? 'http://localhost:7071/' : '';
@@ -18,12 +18,16 @@ function App(props) {
 
   useEffect(() => {
     (async () => {
-      if (!isLoading) return;
+      if (user != null) return;
       try {
         const data = await fetch('.auth/me');
         const json = await data.json();
         console.log(json);
-        setUser(new User(json.clientPrincipal.userId, json.clientPrincipal.userDetails));
+        if (json.clientPrincipal != null) {
+          setUser(new User(json.clientPrincipal.userId, json.clientPrincipal.userDetails));
+        } else {
+          setUser(null);
+        }
         setIsLoading(false);
       } catch (e) {
         console.log(e);
@@ -36,8 +40,9 @@ function App(props) {
   }
 
   if (!user) {
-    window.location = '/.auth/login/github';
-    return <div></div>
+    // window.location = '/.auth/login/github';
+    window.history.pushState({}, undefined, '/login');
+    // return <div></div>
   }
 
   return (
@@ -46,18 +51,42 @@ function App(props) {
         <header className="App-header">
           Welcome to Vaesen RPG
         </header>
-        <ApiMessage></ApiMessage>
-        <UserInfo user={ user }></UserInfo>
         <Routes>
-          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path='/' element={<Home user={user}/>} />
+          <Route exact path='login' element={<Login/>} />
+          <Route exact path='*' element={<div>No Path</div>} />
         </Routes>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return <div>Welcome to Vaesen...</div>
+function Login() {
+  return (
+    <div>
+      Choose login method:
+      <a onClick={() => { window.location = '/.auth/login/github' }}>Github</a>
+    </div>
+  )
+}
+
+function Home({user}) {
+  const logout = () => {
+    // window.history.pushState({}, undefined, '/.auth/logout');
+    window.location = '.auth/logout'
+  }
+
+  return (
+    <>
+      <div><a onClick={logout}>Logout</a></div>
+      <ApiMessage></ApiMessage>
+      <UserInfo user={user}></UserInfo>
+    </>
+  )
+}
+
+Home.propTypes = {
+  user: PropTypes.instanceOf(User)
 }
 
 export function ApiMessage() {
@@ -81,7 +110,7 @@ export function ApiMessage() {
   return <div>Message: {data}</div>
 }
 
-export function UserInfo({ user }) {
+export function UserInfo({user}) {
   return (
     <div>
       <div>User ID: {user.userId}</div>
